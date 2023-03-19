5374caf49998aa8c16431f50b5c9ad88dfabac64
Ahmed Chebbi <chebbi.ahmed.info@gmail.com>
2023-03-19 13:25:42 +0100

    configure imports for app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
 import { ProjectsModule } from './projects/projects.module';
 import { PrivilegesModule } from './privileges/privileges.module';
 import { UserRoleProjectModule } from './user-role-project/user-role-project.module';
 import { RolePrivilegeModule } from './role-privilege/role-privilege.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [AuthModule, RolesModule, ProjectsModule, PrivilegesModule, UserRoleProjectModule, RolePrivilegeModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    RolesModule,
+    ProjectsModule,
+    PrivilegesModule,
+    UserRoleProjectModule,
+    RolePrivilegeModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
