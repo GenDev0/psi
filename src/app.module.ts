1094d33005a54c47a403715bdd19de1196304049
Ahmed Chebbi <chebbi.ahmed.info@gmail.com>
2023-03-19 13:08:10 +0100

    add privileges ressource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { RolesModule } from './roles/roles.module';
 import { ProjectsModule } from './projects/projects.module';
+import { PrivilegesModule } from './privileges/privileges.module';
 
 @Module({
-  imports: [AuthModule, RolesModule, ProjectsModule],
+  imports: [AuthModule, RolesModule, ProjectsModule, PrivilegesModule],
   controllers: [AppController],
   providers: [AppService],
 })
