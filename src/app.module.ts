ae8cd1c4f488ac615d023ed3d2576611149c3840
Ahmed Chebbi <chebbi.ahmed.info@gmail.com>
2023-03-19 13:06:32 +0100

    add roles ressource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
